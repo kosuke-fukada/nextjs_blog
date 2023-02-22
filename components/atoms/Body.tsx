253913c7ac5c2d6e38ac69627a6cf67dde51a112
Kosuke Fukada <kosuke.fukada@smaregi.jp>
2023-02-22 21:27:09 +0900

    added code function to Body component.

--- components/atoms/Body.tsx
+++ components/atoms/Body.tsx
@@ -1,16 +1,35 @@
 import ReactMarkdown from 'react-markdown';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atelierHeathDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import LinkCard from './LinkCard';
 
 const Body = (props: { body: string }): JSX.Element => {
-  
+
   return (
     <div id='content'>
       <ReactMarkdown
         children={props.body}
         components={{
-          a({href}) {
-            return (
-              <LinkCard link={href} />
+          // TODO: Hydration error
+          // a({href}) {
+          //   return (
+          //     <LinkCard link={href} />
+          //   )
+          // },
+          code({ node, inline, className, children, style, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                style={atelierHeathDark}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
             )
           }
         }}
